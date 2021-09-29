06d4acf8988f4666f5ae5be955395bdea2f5656c
Sobhi <sub7i.alkhuder@gmail.com>
2021-09-29 19:16:54 +0200

    Refactor async functions

--- api/controllers/getMsgs.js
+++ api/controllers/getMsgs.js
@@ -1,14 +1,19 @@
-import getUserMessages from '../../services/processMsgs.js';
+import getUserMsgs from '../../services/processMsgs.js';
 import { oAuth2Client } from '../../config/auth.js'
 
 export default async function getMessages(req, res) {
+  const { userId } = req
+
+  if (!userId) {
+    res.status(401).json("Wrong credentials!")
+    return
+  }
+
   try {
-    const { userId } = req
-    const messages = await getUserMessages(oAuth2Client, userId)
+    const messages = await getUserMsgs(oAuth2Client, userId)
 
     return res.status(200).json({ messages })
-
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(500).json(error.message)
   }
 }
